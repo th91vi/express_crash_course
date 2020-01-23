faf046a93b35d47d5a158af45a028196fd39a093
Thiago Vieira Pereira <thiagov@ALFAMA.COM>
2020-01-23 17:37:33 -0300

    ajuste no array members, ajuste na função de put, apaga member

--- routes/api/members.js
+++ routes/api/members.js
@@ -38,23 +38,17 @@ router.post('/', (req, res) => {
     name: req.body.name,
     username: req.body.username,
     email: req.body.email,
-    address: {
-      street: req.body.address.street,
-      suite: req.body.address.suite,
-      city: req.body.address.city,
-      zipcode: req.body.address.zipcode,
-      geo: {
-        lat: req.body.address.geo.lat,
-        lng: req.body.address.geo.lng
-      }
-    },
+    street: req.body.street,
+    suite: req.body.suite,
+    city: req.body.city,
+    zipcode: req.body.zipcode,
+    lat: req.body.lat,
+    lng: req.body.lng,
     phone: req.body.phone,
     website: req.body.website,
-    company: {
-      name: req.body.company.name,
-      catchPhrase: req.body.company.catchPhrase,
-      bs: req.body.company.bs
-    }
+    companyName: req.body.companyName,
+    companyCatchPhrase: req.body.companyCatchPhrase,
+    companyBs: req.body.companyBs
   };
 
   if (!newMember.name || !newMember.email) {
@@ -78,37 +72,34 @@ router.put('/:id', (req, res) => {
     members.forEach(member => {
       // se id do member for igual ao passado como parametro
       if(member.id === parseInt(req.params.id)){
-        // condicional ternaria, onde se o campo atualizado foi enviado na requisição, recebe o valor atualizado; senão recebe o valor atual, não atualizando (dããã!)
 
-        // ### consertar sintaxe
-
-
-        // member.name = updMember.name ? updMember.name : member.name,
-        // member.username = updMember.username ? updMember.username : member.username,
-        // member.email = updMember.email ? updMember.email : member.email,
-        // member.address = {
-        //   street : updMember.address.street ? updMember.address.street : member.address.street,
-        //   suite : updMember.address.suite ? updMember.address.suite : member.address.suite,
-        //   city : updMember.address.city ? updMember.address.city : member.address.city,
-        //   zipcode : updMember.address.zipcode ? updMember.address.zipcode : member.address.zipcode,
-        //   geo : {
-        //     lat : updMember.address.geo.lat ? updMember.address.geo.lat : member.address.geo.lat,
-        //     lng : updMember.address.geo.lng ? updMember.address.geo.lng : member.address.geo.lng,
-        //   }
-        // },
-        // member.phone = updMember.phone ? updMember.phone : member.phone,
-        // member.website = updMember.phone ? updMember.phone : member.phone,
-        // member.company = {
-        //   name = updMember.company.name ? updMember.company.name : member.company.name,
-        //   catchPhrase = updMember.company.catchPhrase ? updMember.company.catchPhrase : member.company.catchPhrase,
-        //   bs = updMember.company.bs ? updMember.company.bs : member.company.bs,
-        // }
+        // atribuição dentre campos usando destructuring
+        member = { ...member, ...updMember }
 
         // resposta após atualização
         // abaixo, 'member:member' é o mesmo que 'member'
         res.json({msg: 'Member updated', member})
       }
     })
+  } else {
+    // se nao houver membro com a id especificada, manda um status 400
+    res.status(400).json({
+      msg: `No member with the id ${req.params.id} found...`
+    });
+  }
+});
+
+// apaga member
+router.delete('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id))
+
+  if (found) {
+    // res.send(req.params.id); // retorna o numero da id do usuario passado na url
+    // abaixo, 'req.params.id' esta dentro de um parseInt pois seu tipo inicial eh uma string
+    res.json({
+      msg: 'Member deleted',
+      members: members.filter(member => member.id !== parseInt(req.params.id))
+    });
   } else {
     // se nao houver membro com a id especificada, manda um status 400
     res.status(400).json({
